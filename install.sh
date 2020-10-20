e545afb939ae95fbcb3d4c99c037261c271266ae
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-10-20 10:45:02 +0900

    fixed p10k to p10k.zsh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 main () {
   SCRIPT_DIR=$(cd $(dirname $0); pwd)
-  DOT_FILES=(.gitconfig .tmux.conf .zshrc .p10k)
+  DOT_FILES=(.gitconfig .tmux.conf .zshrc .p10k.zsh)
 
   for file in ${DOT_FILES[@]}
   do
