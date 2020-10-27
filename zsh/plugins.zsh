bf65076d21b156a9b70d008b0240cbd481e488fb
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-10-27 10:49:44 +0900

    added fzf keybinds

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,10 +1,14 @@
-# p10k
+#############################################################################################
+######################################## p10k ###############################################
+#############################################################################################
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 # # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 
-# exa
+#############################################################################################
+####################################### exa #################################################
+#############################################################################################
 zinit ice as"program" from"gh-r" mv"exa* -> exa"
 zinit light ogham/exa
 if builtin command -v exa > /dev/null; then
@@ -13,8 +17,22 @@ if builtin command -v exa > /dev/null; then
   alias ls="exa --icons"
 fi
 
+#############################################################################################
+################################### syntax highlight ########################################
+#############################################################################################
+zinit light zdharma/fast-syntax-highlighting
 
-# bat
+
+#############################################################################################
+##################################### auto suggest ##########################################
+#############################################################################################
+zinit light zsh-users/zsh-autosuggestions
+bindkey '^j' autosuggest-accept
+
+
+#############################################################################################
+####################################### bat #################################################
+#############################################################################################
 zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
 zinit light sharkdp/bat
 if builtin command -v bat > /dev/null; then
@@ -22,7 +40,9 @@ if builtin command -v bat > /dev/null; then
 fi
 
 
-# fzf
+#############################################################################################
+####################################### fzf #################################################
+#############################################################################################
 zinit ice from"gh-r" as"program"
 zinit load junegunn/fzf-bin
 export FZF_DEFAULT_OPTS="--reverse --inline-info --height 70% --border"
@@ -44,10 +64,51 @@ fadd() {
   done
 }
 alias fa="fadd"
+__fzfcmd() {
+  [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; } &&
+    echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
+}
+# ALT-C - cd into the selected directory
+fzf-cd-widget() {
+  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type d -print 2> /dev/null | cut -b3-"}"
+  setopt localoptions pipefail no_aliases 2> /dev/null
+  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+  if [ -z "$BUFFER" ]; then
+    BUFFER="cd ${(q)dir}"
+    zle accept-line
+  else
+    print -sr "cd ${(q)dir}"
+    cd "$dir"
+  fi
+  local ret=$?
+  unset dir # ensure this doesn't end up appearing in prompt expansion
+  zle fzf-redraw-prompt
+  return $ret
+}
+zle     -N    fzf-cd-widget
+bindkey '\ec' fzf-cd-widget
+# CTRL-R - Paste the selected command from history into the command line
+fzf-history-widget() {
+  local selected num
+  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+  selected=( $(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
+    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
+  local ret=$?
+  if [ -n "$selected" ]; then
+    num=$selected[1]
+    if [ -n "$num" ]; then
+      zle vi-fetch-history -n $num
+    fi
+  fi
+  zle reset-prompt
+  return $ret
+}
+zle     -N   fzf-history-widget
+bindkey '^R' fzf-history-widget
 
 
-zinit light zdharma/fast-syntax-highlighting
-
-
-zinit light zsh-users/zsh-autosuggestions
-bindkey '^j' autosuggest-accept
