59cec95d26e4378b35b3f7f3ec82539463b3d8ed
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-11-04 11:55:39 +0900

    added screen shot

--- initialize.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-install_dein (){
-  mkdir -p $HOME/.config/nvim/dein/repos/github.com/Shougo/dein.vim
-  git clone https://github.com/Shougo/dein.vim.git $HOME/.config/nvim/dein/repos/github.com/Shougo/dein.vim
-}
-
-install_dein
--- nvim/README.md
+++
@@ -1,7 +0,0 @@
-deinの設定はちょっと独自
-
-```
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-# プラグインのインストール先を~/config/nvim/dein以下に設定している．
-sh ./installer.sh ~/.config/nvim/dein
-```
