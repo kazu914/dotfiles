4436f2fc410a63122d5067d200a7ff052af3c351
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-11-04 11:29:08 +0900

    added setting to completion

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -29,6 +29,14 @@ autoload -Uz compinit && compinit
 # 補完候補をそのまま探す -> 小文字を大文字に変えて探す -> 大文字を小文字に変えて探す
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}'
 
+### 補完方法毎にグループ化する。
+zstyle ':completion:*' format '%B%F{blue}%d%f%b'
+zstyle ':completion:*' group-name ''
+
+
+### 補完侯補をメニューから選択する。
+### select=2: 補完候補を一覧から選択する。補完候補が2つ以上なければすぐに補完する。
+zstyle ':completion:*:default' menu select=2
 #################################  OTHERS  #################################
 # automatically change directory when dir name is typed
 setopt auto_cd
