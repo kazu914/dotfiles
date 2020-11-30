7625e5801412ae2292dc7e4f374350b4eda31ad4
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-11-30 11:06:52 +0900

    added tmux plugin manager

--- initialize.sh
+++ initialize.sh
@@ -4,4 +4,10 @@ install_dein (){
   git clone https://github.com/Shougo/dein.vim.git $HOME/.config/nvim/dein/repos/github.com/Shougo/dein.vim
 }
 
+install_tpm (){
+  mkdir -p $HOME/.tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+}
+
 install_dein
+install_tpm
