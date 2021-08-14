8e0e7c0bee9116a310c117a024663bb805436777
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-08-14 16:11:42 +0900

    :lipstick: (zsh) add color to fd

--- zsh/02_aliases.zsh
+++ zsh/02_aliases.zsh
@@ -24,14 +24,14 @@ vim (){
   empty="(Empty)"
 
   if [ $# = 0 ];then
-    candidate=`fd -t f`
+    candidate=`fd --color=always -t f`
     candidate=($empty"\n"${candidate[@]})
     selected=$(echo $candidate | fzf --preview "if [ {} = '$empty' ]; then echo 'Open Here'; else bat  --color=always --style=header,grid --line-range :100 {}; fi")
   else
     if [ -f $1 ]; then
       selected=$1
     else
-      candidate=`fd -t f`
+      candidate=`fd --color=always -t f`
       candidate=($1"\n"${candidate[@]})
       selected=$(echo $candidate | fzf --query "$1" --preview "if [ {} = '$1' ]; then echo 'Create new file: $1'; else bat  --color=always --style=header,grid --line-range :100 {}; fi")
     fi
@@ -51,12 +51,12 @@ vim (){
 cat (){
   local selected
   if [ $# = 0 ];then
-    selected=$(fd -t f | fzf --preview "bat  --color=always --style=header,grid --line-range :100 {}")
+    selected=$(fd --color=always -t f | fzf --preview "bat  --color=always --style=header,grid --line-range :100 {}")
   else
     if [ -f $1 ]; then
       selected=$1
     else
-      selected=$(fd -t f | fzf --query "$1" --preview "bat  --color=always --style=header,grid --line-range :100 {}")
+      selected=$(fd --color=always -t f | fzf --query "$1" --preview "bat  --color=always --style=header,grid --line-range :100 {}")
     fi
   fi
 
