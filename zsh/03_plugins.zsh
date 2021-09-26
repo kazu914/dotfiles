7a5dc2680d67530ce2048df06d945bfb25e5dcfc
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-09-26 15:26:28 +0900

    :sparkles: (zsh) add fzf-tab

--- zsh/03_plugins.zsh
+++ zsh/03_plugins.zsh
@@ -85,3 +85,18 @@ zinit load wfxr/code-minimap
 #############################################################################################
 zinit ice wait lucid as"program" from"gh-r" mv"fd* -> fd" pick"fd/fd"
 zinit light sharkdp/fd
+
+#############################################################################################
+##################################### fzf-tab ###############################################
+#############################################################################################
+zinit light Aloxaf/fzf-tab
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+# switch group using `,` and `.`
+zstyle ':fzf-tab:*' switch-group ',' '.'
