1df593ae6225ad998ec6f9cae7b5afe3903f4e56
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-11-27 13:29:42 +0900

    :sparkles: (use cmp instead of ddc)

--- nvim/plugins/lsp_setting.lua
+++ nvim/plugins/lsp_setting.lua
@@ -22,8 +22,9 @@ end
 
 local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.on_server_ready(function(server)
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   capabilities.textDocument.completion.completionItem.snippetSupport = true
+
   local opts = {
     on_attach = on_attach,
     capabilities = capabilities
