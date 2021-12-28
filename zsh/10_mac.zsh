e40e66269199839d5443c666737789d16d546dfc
kazu914 <nkz0914ssk@gmail.com>
2021-12-28 22:57:26 +0900

    (hammerspoon) remove fzf bookmark

--- zsh/10_mac.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function fzf_bookmarks() {
-  candidate1=`\cat ${HOME}/Library/Application\ Support/Google/Chrome/Default/Bookmarks | jq -r '.roots.bookmark_bar as $p | $p.children[] | select(has("url")|not) | .children[] |  .name + " | " + .url'`
-  candidate2=`\cat ${HOME}/Library/Application\ Support/Google/Chrome/Default/Bookmarks | jq -r '.roots.bookmark_bar as $p | $p.children[] | select(has("url")) | .name + " | " + .url'`
-  candidate=( "${candidate1[@]}" "${candidate2[@]}" )
-  selected=$(echo "$candidate" | $HOME/.zinit/plugins/junegunn---fzf/fzf)
-  open -a 'Google Chrome' `echo ${selected##*|}`
-}
