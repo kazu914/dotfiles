0ee1f34f18d70d262bbfeb66686e63edafcde13f
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-12-28 10:01:33 +0900

    remove d function

--- zsh/10_mac.zsh
+++ zsh/10_mac.zsh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-function fzf_launch() {
-  local applications
-  applications="$(find /Applications /System/Applications -name "*app" -maxdepth 2)"
-  open -a "$( printf '%s\n' "${applications[@]}" | awk -F/ '{print $NF}' | sed 's/\.app//'| sort | $HOME/.zinit/plugins/junegunn---fzf/fzf)"
-}
-
 function fzf_bookmarks() {
   candidate1=`\cat ${HOME}/Library/Application\ Support/Google/Chrome/Default/Bookmarks | jq -r '.roots.bookmark_bar as $p | $p.children[] | select(has("url")|not) | .children[] |  .name + " | " + .url'`
   candidate2=`\cat ${HOME}/Library/Application\ Support/Google/Chrome/Default/Bookmarks | jq -r '.roots.bookmark_bar as $p | $p.children[] | select(has("url")) | .name + " | " + .url'`
@@ -13,9 +7,3 @@ function fzf_bookmarks() {
   selected=$(echo "$candidate" | $HOME/.zinit/plugins/junegunn---fzf/fzf)
   open -a 'Google Chrome' `echo ${selected##*|}`
 }
-
-if [[ $OSTYPE == 'darwin'* ]]; then
-  alias d=fzf_launch
-fi
-
-
