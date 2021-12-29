4aae3778504a4569845c0fbbbbdd70b6716019f1
kazu914 <nkz0914ssk@gmail.com>
2021-12-29 18:23:16 +0900

    (hammerspoon) add window handling key-bind

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -63,16 +63,6 @@ hs.hotkey.bind({"cmd"}, "d", function()
 end)
 
 -- [[
--- Window Switcher
--- ]]
-local switcher = hs.window.switcher.new(hs.window.filter.new():setSortOrder(hs.window.filter.sortByCreated):setDefaultFilter{})
-switcher.ui.showSelectedThumbnail = false
-switcher.ui.showThumbnails = false
-hs.hotkey.bind('cmd','l',function()switcher:next()end,nil,function()switcher:next()end)
-hs.hotkey.bind('cmd','h',function()switcher:previous()end,nil,function()switcher:previous()end)
-
-
--- [[
 -- choose and open a bookmark
 -- ]]
 hs.hotkey.bind({"cmd"}, "b", function()
@@ -108,3 +98,57 @@ function extract_bookmarks(items,path)
     return bookmarks
   end
 end
+
+-- [[
+-- move focused window to previous mointor
+-- ]]
+hs.hotkey.bind({'shift', 'cmd'}, 'h', function()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  win:move(win:frame():toUnitRect(screen:frame()), screen:previous(), true, 0)
+end)
+
+-- [[
+-- move focused window to next mointor
+-- ]]
+hs.hotkey.bind({'shift', 'cmd'}, 'l', function()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
+
+-- [[
+-- focuse next window
+-- ]]
+hs.hotkey.bind({'cmd'}, 'l', function()
+  local allWindows = hs.window.allWindows()
+  local focusedWindow = hs.window.focusedWindow()
+  for k,v in pairs(allWindows)do
+    if v == focusedWindow then
+      if k == #allWindows then
+        allWindows[1]:focus()
+      else
+        allWindows[k + 1]:focus()
+      end
+    end
+  end
+end)
+
+-- [[
+-- focuse previous window
+-- ]]
+hs.hotkey.bind({'cmd'}, 'h', function()
+  local allWindows = hs.window.allWindows()
+  local focusedWindow = hs.window.focusedWindow()
+  for k,v in pairs(allWindows)do
+    if v == focusedWindow then
+      if k == 1 then
+        allWindows[#allWindows]:focus()
+      else
+        allWindows[k-1]:focus()
+      end
+    end
+  end
+end)
+
+
