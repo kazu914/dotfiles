19eed07bfbe8dc42e5da3732d7bcd134089d2f86
kazu914 <nkz0914ssk@gmail.com>
2021-12-30 23:59:39 +0900

    (hammerspoon) refactored

--- hammerspoon/ApplicationLaucher.lua
+++ hammerspoon/ApplicationLaucher.lua
@@ -12,8 +12,8 @@ local function filter(choices, query)
     if string.len(query) == 0 then return choices end
     local filtered_apps = {}
     for _, app in pairs(choices) do
-        local matched_idxs = FuzzyMatcher.fuzzyMatch(app.text:getString(),
-                                                      query)
+        local matched_idxs =
+            FuzzyMatcher.fuzzyMatch(app.text:getString(), query)
         if #matched_idxs ~= 0 then
             local new_app = {
                 text = highlightMatched(app.text:getString(), matched_idxs),
@@ -25,40 +25,44 @@ local function filter(choices, query)
     return filtered_apps
 end
 
+local function getAppsInPath(path)
+    local apps = {}
+    local list = hs.execute('ls ' .. path)
+    for token in string.gmatch(list, "[^\r\n]+") do
+        table.insert(apps,
+                     {text = hs.styledtext.new(token), subText = path .. token})
+    end
+    return apps
+end
+
+local function getApps()
+    local apps = {}
+    for _, path in pairs({"/Applications", "~/Applications"}) do
+        for _, app in pairs(getAppsInPath(path)) do
+            table.insert(apps, app)
+        end
+    end
+    return apps
+end
+
+local function setChoices(apps, chooser)
+    local filtered_apps = filter(apps, chooser:query())
+    chooser:choices(filtered_apps)
+    chooser:refreshChoicesCallback(true)
+end
+
 -- [[
 -- choose and open an Application
 -- ]]
 hs.hotkey.bind({"cmd"}, "d", function()
-    local choices = {}
-    local list = hs.execute('ls /Applications')
-    for token in string.gmatch(list, "[^\r\n]+") do
-        table.insert(choices, {
-            text = hs.styledtext.new(token),
-            subText = "/Applications/" .. token
-        })
-    end
+    local apps = getApps()
 
-    local list = hs.execute('ls ~/Applications')
-    for token in string.gmatch(list, "[^\r\n]+") do
-        table.insert(choices, {
-            text = hs.styledtext.new(token),
-            subText = "~/Applications/" .. token
-        })
-    end
-
-    local chooser = hs.chooser.new(function(choice)
-        hs.application.launchOrFocus(choice.text:getString())
+    local chooser = hs.chooser.new(function(app)
+        hs.application.launchOrFocus(app.text:getString())
         hs.window.focusedWindow():maximize()
     end)
 
-    local function generateChoices()
-        local filtered_apps = filter(choices, chooser:query())
-        chooser:choices(filtered_apps)
-        chooser:refreshChoicesCallback(true)
-    end
-
-    chooser:choices(generateChoices)
-    chooser:queryChangedCallback(generateChoices)
+    setChoices(apps, chooser)
+    chooser:queryChangedCallback(function() setChoices(apps, chooser) end)
     chooser:show()
 end)
-
