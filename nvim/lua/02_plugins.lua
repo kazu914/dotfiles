891502bdb7b7737aac4abe36664993baaea53e4d
Kazuaki Nomura <nkz0914ssk@gmail.com>
2022-05-16 07:56:14 +0900

    (nvim) toggleterm: set false to start in inseart

--- nvim/lua/02_plugins.lua
+++ nvim/lua/02_plugins.lua
@@ -232,6 +232,7 @@ require 'packer'.startup(function(use)
     require("toggleterm").setup{
       size = vim.o.columns * 0.4,
       direction = 'vertical',
+      start_in_insert = false,
     }
     vim.keymap.set('n', '<C-o>', ':ToggleTerm<CR>', { noremap = true })
     vim.keymap.set('n', '<C-o>', ':ToggleTerm<CR>', { noremap = true })
