af437897a69194262c10dda931abe57aa1edc10c
Kazuaki Nomura <nkz0914ssk@gmail.com>
2022-05-26 19:22:46 +0900

    (wezterm) add active copy mode key bind

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -64,6 +64,10 @@ local key_bindings = {
         key = "x",
         mods = "LEADER",
         action = wezterm.action {CloseCurrentTab = {confirm = true}}
+    }, {
+        key = "[",
+        mods = "LEADER",
+        action = "ActivateCopyMode"
     },
     {key = "l", mods = "ALT", action = wezterm.action {ActivateTabRelative = 1}},
     {
