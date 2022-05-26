ca4de0df8ccf2484067af84099eab1cecda59733
Kazuaki Nomura <nkz0914ssk@gmail.com>
2022-05-27 08:33:15 +0900

    (wezterm) bug fix in clipboard has no value

--- wezterm/utils.lua
+++ wezterm/utils.lua
@@ -8,19 +8,17 @@ M.merge_tables = function(table_a, table_b)
 end
 
 M.trim_whitespace = function(s)
-    s = s:gsub("*", "aa")
+    if s == nil then return "" end
     return s:match '^%s*(.*%S)' or ''
 end
 
-M.concat_first_and_last_line = function (lists)
-  if #lists == 1 then
-    return M.trim_whitespace(lists[1])
-  end
+M.concat_first_and_last_line = function(lists)
+    if #lists == 1 then return M.trim_whitespace(lists[1]) end
 
-  local first = lists[1]
-  local last = lists[#lists]
+    local first = lists[1]
+    local last = lists[#lists]
 
-  return M.trim_whitespace(first) .. " .. " .. M.trim_whitespace(last)
+    return M.trim_whitespace(first) .. " .. " .. M.trim_whitespace(last)
 end
 
 return M
