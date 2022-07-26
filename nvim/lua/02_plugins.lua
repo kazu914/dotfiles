3c2244a09b17a92a968f3252763fc54ec16086ee
Kazuaki Nomura <nkz0914ssk@gmail.com>
2022-07-27 08:11:25 +0900

    (nvim) remove context

--- nvim/lua/02_plugins.lua
+++ nvim/lua/02_plugins.lua
@@ -38,8 +38,6 @@ require 'packer'.startup(function(use)
   use { "lukas-reineke/indent-blankline.nvim", config = function()
     require("indent_blankline").setup {
       space_char_blankline = " ",
-      show_current_context = true,
-      show_current_context_start = true,
     }
   end }
 
