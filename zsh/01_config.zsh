3251bf864a8e45783361b4a1cc8e3a90fe9ebb43
Kazuaki Nomura <nkz0914ssk@gmail.com>
2022-08-14 14:06:34 +0900

    (zsh) remove mode display

--- zsh/01_config.zsh
+++ zsh/01_config.zsh
@@ -63,12 +63,3 @@ export VISUAL=nvim
 
 set -o vi
 bindkey "jj" vi-cmd-mode
-function zle-line-init zle-keymap-select {
-    VIM_NORMAL="%K{208}%F{black}%k%f%K{208}%F{white} % NORMAL %k%f%K{black}%F{208}%k%f"
-    VIM_INSERT="%K{075}%F{black}%k%f%K{075}%F{white} % INSERT %k%f%K{black}%F{075}%k%f"
-    RPS1="${${KEYMAP/vicmd/$VIM_NORMAL}/(main|viins)/$VIM_INSERT}"
-    RPS2=$RPS1
-    zle reset-prompt
-}
-zle -N zle-line-init
-zle -N zle-keymap-select
