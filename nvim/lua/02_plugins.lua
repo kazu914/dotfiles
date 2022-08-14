467070e99ab2015f29d02472372639909bdb4394
kazu914 <nkz0914ssk@gmail.com>
2022-08-14 15:28:39 +0900

    (nvim) update scrollbar config

--- nvim/lua/02_plugins.lua
+++ nvim/lua/02_plugins.lua
@@ -86,18 +86,7 @@ require 'packer'.startup(function(use)
 
   use { 'petertriho/nvim-scrollbar',
     config = function()
-      local colors = require("tokyonight.colors").setup()
-      require("scrollbar").setup({
-        handle = { color = colors.bg_highlight },
-        marks = {
-          Search = { color = colors.orange },
-          Error = { color = colors.error },
-          Warn = { color = colors.warning },
-          Info = { color = colors.info },
-          Hint = { color = colors.hint },
-          Misc = { color = colors.purple }
-        }
-      })
+      require("scrollbar").setup({})
       require("scrollbar.handlers.search").setup()
     end
   }
