f3bb845d806f4c254dc19527058e78c5e5bb6cb3
Kazuaki Nomura <nkz0914ssk@gmail.com>
2022-08-18 08:33:53 +0900

    (zsh) add tmp alias

--- zsh/02_aliases.zsh
+++ zsh/02_aliases.zsh
@@ -25,15 +25,7 @@ cd (){
   __zoxide_z "$@" && pwd && lsd --tree -a --depth=1 
 }
 
-
-tmp (){
-  if [ -z $1 ]; then
-    filename=${HOME}/tmp/`date +%Y_%m_%d_%H_%M_%S`.txt
-  else
-    filename=${HOME}/tmp/`date +%Y_%m_%d_%H_%M`_$1
-  fi
-  vim ${filename}
-}
+alias tmp='kakisute'
 
 if builtin command -v gsed > /dev/null; then
   alias sed='gsed'
