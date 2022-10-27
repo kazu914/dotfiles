dc7c98e2dd1417269ee2900dea8281dd964dee8a
Kazuaki Nomura <nkz0914ssk@gmail.com>
2022-10-28 08:49:28 +0900

    (nvim) add nil check

--- nvim/lua/plugins_config/nvim-jdtls.lua
+++ nvim/lua/plugins_config/nvim-jdtls.lua
@@ -20,7 +20,7 @@ function M.setup()
 
   local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
   local root_dir = require("jdtls.setup").find_root(root_markers)
-  if root_dir == "" then
+  if root_dir == "" or root_dir == nil then
     return
   end
 
