025da719d0b6ca0d7bd6b77ae7527526d1ad1ecd
Kazuaki Nomura <nkz0914ssk@gmail.com>
2022-12-05 08:53:50 +0900

    (nvim) modify diagnostic format

--- nvim/lua/03_lsp_setting.lua
+++ nvim/lua/03_lsp_setting.lua
@@ -132,4 +132,14 @@ for type, icon in pairs(diagnostic_icons) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl })
 end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  update_in_insert = false,
+  virtual_text = {
+    format = function(diagnostic)
+      return string.format("%s 【%s: %s】", diagnostic.message, diagnostic.source, diagnostic.code)
+    end,
+  },
+})
+
 return M
