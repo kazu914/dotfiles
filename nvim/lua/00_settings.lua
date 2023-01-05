bd258bfdf28d34c4973258190b91f350fd8e488b
kazu914 <nkz0914ssk@gmail.com>
2023-01-05 22:03:33 +0900

    (nvim) use lua for options

--- nvim/lua/00_settings.lua
+++ nvim/lua/00_settings.lua
@@ -1,51 +1,31 @@
-vim.cmd([[
-syntax on
-set t_Co=256
-
-set autoindent                    " 改行時に自動でインデント
-set smartindent
-set expandtab                     " タブ入力を空白に置換
-set tabstop=2                     " タブを何文字分の空白にするか
-set shiftwidth=2                  " 自動インデント時に入力する空白の数
-set cursorline                    " カーソルラインの表示
-set number                        " 行番号の表示
-set showtabline=2
-set showmatch
-set title
-set backspace=indent,eol,start
-set inccommand=split
-set imdisable
-set hidden
-set nobackup
-set nowritebackup
-set termguicolors
-set ignorecase
-set smartcase
-set conceallevel=0
-set encoding=utf-8
-set fileencoding=utf-8
-set fileencodings=utf-8,iso-2022-jp,euc-jp,ucs-2,cp932,sjis
-set clipboard=unnamedplus         " クリップボード連携
-set mouse=
-set noswapfile
-set pumblend=10
-set winblend=10
-set foldmethod=syntax
-set nofoldenable
-set signcolumn=yes:2
-set cmdheight=0
-
-hi Comment ctermfg=gray
-
-let g:netrw_liststyle=1
-let g:netrw_banner=0
-let g:netrw_sizestyle="H"
-let g:netrw_timefmt="%Y/%m/%d(%a) %H:%M:%S"
-let g:netrw_preview=1
-
-if has('persistent_undo')
-    set undodir=~/.config/nvim/undo
-    set undofile
-endif
-
-]])
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.cursorline = true
+vim.opt.showtabline = 2
+vim.opt.showmatch = true
+vim.opt.number = true
+vim.opt.title = true
+vim.opt.backspace = "indent,eol,start"
+vim.opt.inccommand = "split"
+vim.opt.imdisable = true
+vim.opt.hidden = true
+vim.opt.termguicolors = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.conceallevel = 0
+vim.opt.encoding="utf-8"
+vim.opt.fileencoding="utf-8"
+vim.opt.fileencodings="utf-8,iso-2022-jp,euc-jp,ucs-2,cp932,sjis"
+vim.opt.clipboard="unnamedplus"
+vim.opt.swapfile=false
+vim.opt.pumblend=10
+vim.opt.winblend=10
+vim.opt.foldmethod="syntax"
+vim.opt.foldenable=false
+vim.opt.signcolumn="yes:2"
+vim.opt.cmdheight=0
+vim.opt.undodir="~/.config/nvim.undo"
+vim.opt.undofile=true
