8cf7a362ee0daf1c23fc1a8201271fb31755b260
kazu914 <nkz0914ssk@gmail.com>
2023-01-07 21:38:51 +0900

    (nvim) use lua in mapping setting

--- nvim/lua/01_mappings.lua
+++ nvim/lua/01_mappings.lua
@@ -1,71 +1,48 @@
-vim.cmd([[
-let mapleader = "\<Space>"
-" タブ関連
-" The prefix key.
-nnoremap    [Tag]   <Nop>
-nmap    t [Tag]
-" Tab jump
-" t1 で1番左のタブ、t2 で1番左から2番目のタブにジャンプ
-for n in range(1, 9)
-  execute 'nnoremap <silent> [Tag]'.n  ':<C-u>tabnext'.n.'<CR>'
-endfor
-" tc 新しいタブを一番右に作る
-map <silent> [Tag]c :tablast <bar> tabnew<CR>
-" tx タブを閉じる
-map <silent> [Tag]x :tabclose<CR>
-" tn 次のタブ
-map <silent> [Tag]n :tabnext<CR>
-" tp 前のタブ
-map <silent> [Tag]p :tabprevious<CR>
+vim.g.mapleader = ' '
 
-" PageUp,PageDownを無効化
-noremap <PageUp> <Nop>
-noremap <PageDown> <Nop>
-inoremap <PageUp> <Nop>
-inoremap <PageDown> <Nop>
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
 
-" カーソル下の単語ハイライト
-nnoremap <silent> <Space><Space> "zyiw:let @/ = '\<' . @z . '\>'<CR>:set hlsearch<CR>
+-- PageUp,PageDownを無効化
+map({ 'n', 'i' }, '<PageUp>', '<nop>')
+map({ 'n', 'i' }, '<PageDown>', '<nop>')
+-- カーソル下の単語ハイライト
+map('n', '<Space><Space>', '"zyiw:let @/ = @z <CR>:set hlsearch<CR>')
 
-" カーソル下の単語を検索置換
-nmap # <Space><Space>:%s/<C-r>///g<Left><Left>
+-- x,s ではヤンクしない
+map('n', 'x', '"_x')
+map('n', 's', '"_s')
 
-" x,s ではヤンクしない
-nnoremap x "_x
-nnoremap s "_s
+-- コピペのときに自動で対象の末尾に移動
+map('v', 'y', 'y`]')
+map({ 'n', 'v' }, 'p', 'p`]')
 
-" コピペのときに自動で対象の末尾に移動
-vnoremap <silent> y y`]
-vnoremap <silent> p p`]
-nnoremap <silent> p p`]
+-- jjでノーマルモードに戻って保存
+map('i', 'jj', '<Esc>:w<CR>')
 
-" visualモードで連続ペーストする
-xnoremap <expr> p 'pgv"'.v:register.'y`>'
+-- Esc2回押しでハイライト削除
+map('n','<Esc><Esc>', ':nohlsearch<CR>')
+-- 折り返し行移動
+map({'n','v'}, 'j', 'gj')
+map({'n','v'}, 'k', 'gk')
 
-" jjでノーマルモードに戻って保存
-inoremap <silent> jj <Esc>:w<CR>
+-- 保存のマップ
+map('n', '<Leader>w', ':w<CR>')
 
-" Esc2回押しでハイライト削除
-nnoremap <Esc><Esc> :nohlsearch<CR>
+-- 保存のマップ
+map('n', '<Leader>q', ':q<CR>')
 
-" 折り返し行移動
-nnoremap j gj
-nnoremap k gk
-vnoremap j gj
-vnoremap k gk
 
-" 保存のマップ
-nnoremap <silent> <Leader>w :w<CR>
+-- ウィンドウ移動
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
 
-" quit map
-nnoremap <Leader>q :q<CR>
-
-" ウィンドウ移動
-nnoremap <C-h> <C-w>h
-nnoremap <C-j> <C-w>j
-nnoremap <C-k> <C-w>k
-nnoremap <C-l> <C-w>l
-
-" <C-j> で文末にジャンプ，すでに文末なら改行
-inoremap <expr> <C-j>  col(".") == col("$") ? '<C-j>' : '<ESC>A'
-]])
+-- <C-j> で文末にジャンプ，すでに文末なら改行
+map('i', '<C-j>', 'col(".") == col("$") ? \'<C-j>\' : \'<ESC>A\'', {noremap = true, silent = true, expr = true})
