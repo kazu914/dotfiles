b717fef17757e2490094b07879aad9c8b10af782
kazu914 <nkz0914ssk@gmail.com>
2023-04-09 19:15:13 +0900

    (vscode) update keybindings

--- nvim/lua/03_vscode.lua
+++ nvim/lua/03_vscode.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = ' '
 vim.keymap.set("n", "<leader>w", "<Cmd>call VSCodeNotify('workbench.action.files.save')<CR>")
 vim.keymap.set("n", "<leader>q", "<Cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>")
+
+-- カーソル下の単語ハイライト
+vim.keymap.set('n', '<Space><Space>', '"zyiw:let @/ = @z <CR>:set hlsearch<CR>')
