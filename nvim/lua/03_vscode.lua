6c971cf147ae7a99dc27339b50455e3bc3db96ff
Kazuaki Nomura <nkz0914ssk@gmail.com>
2023-04-10 20:03:56 +0900

    (vscode) update keybindings

--- nvim/lua/03_vscode.lua
+++ nvim/lua/03_vscode.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader>q", "<Cmd>call VSCodeNotify('workbench.action.close
 
 -- カーソル下の単語ハイライト
 vim.keymap.set('n', '<Space><Space>', '"zyiw:let @/ = @z <CR>:set hlsearch<CR>')
+
+-- open referenceSearch
+vim.keymap.set('n',"gh","<Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>")
