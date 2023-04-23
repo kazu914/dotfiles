915153aff55893910dbe2fd8c3b46c48ccebbe87
Kazuaki Nomura <nkz0914ssk@gmail.com>
2023-04-23 12:19:27 +0900

    (hammerspoon) format

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,36 +4,36 @@ dofile("BookmarkOpener.lua")
 dofile("TodoManager.lua")
 
 local function openApp(app, path)
-    local application = hs.application.find(app)
-    if application ~= nil and application:isFrontmost() then
-        application:hide()
-    else
-        hs.application.launchOrFocus(path)
-    end
+  local application = hs.application.find(app)
+  if application ~= nil and application:isFrontmost() then
+    application:hide()
+  else
+    hs.application.launchOrFocus(path)
+  end
 end
 
 -- [[
 -- open terminal
 -- ]]
-hs.hotkey.bind({"cmd"}, "space",
-               function() openApp('wezterm', '/Applications/wezterm.app') end)
+hs.hotkey.bind({ "cmd" }, "space",
+  function() openApp('wezterm', '/Applications/wezterm.app') end)
 
 -- [[
 -- open teams
 -- ]]
-hs.hotkey.bind({"cmd"}, "t", function()
-    openApp('teams', '/Applications/Microsoft Teams.app')
+hs.hotkey.bind({ "cmd" }, "t", function()
+  openApp('teams', '/Applications/Microsoft Teams.app')
 end)
 
 -- [[
 -- open google chrome
 -- ]]
-hs.hotkey.bind({"cmd"}, "g", function()
-    openApp("chrome", "/Applications/Google Chrome.app")
+hs.hotkey.bind({ "cmd" }, "g", function()
+  openApp("chrome", "/Applications/Google Chrome.app")
 end)
 
-hs.hotkey.bind({"ctrl"}, "m", function()
-    hs.eventtap.keyStroke(nil, hs.keycodes.map["return"], 0)
+hs.hotkey.bind({ "ctrl" }, "m", function()
+  hs.eventtap.keyStroke(nil, hs.keycodes.map["return"], 0)
 end)
 
 local function file_exists(name)
