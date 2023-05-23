af9c3b40a6aad7f673ba83b5b0a1b9155135e543
Kazuaki Nomura <nkz0914ssk@gmail.com>
2023-05-24 08:33:23 +0900

    (nvim) remove silicon

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -19,16 +19,4 @@ return {
       }
     end
   },
-  {
-    "skanehira/denops-silicon.vim",
-    config = function()
-      vim.g['silicon_options'] = {
-        font = 'Cica',
-        no_line_number = true,
-        background_color = '#434C5E',
-        no_window_controls = true,
-        theme = 'Nord',
-      }
-    end
-  }
 }
