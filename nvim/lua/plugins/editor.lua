301c212465e5c0cc172e8a6b717ecc34377f5c4b
Kazuaki Nomura <nkz0914ssk@gmail.com>
2023-06-03 17:43:46 +0900

    (nvim) use lualine

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -224,4 +224,54 @@ return {
       })
     end
   },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = 'SmiteshP/nvim-navic',
+    config = function()
+      local navic = require("nvim-navic")
+      local colors = {
+        black = '#383a42',
+        white = '#f3f3f3',
+        blue = '#32b9ff',
+        orange = '#ff7832',
+        red = '#ca1243',
+        green = '#8ec07c',
+        yellow = '#ecff32',
+        purple = '#c792ea',
+      }
+      local theme = {
+        normal = {
+          a = { fg = colors.black, bg = colors.blue },
+          b = { fg = colors.black, bg = colors.orange },
+          c = { fg = colors.white, bg = colors.black },
+          x = { fg = colors.white, bg = colors.black },
+          y = { fg = colors.white, bg = colors.red },
+          z = { fg = colors.black, bg = colors.green },
+        },
+        insert = { z = { fg = colors.black, bg = colors.purple } },
+        visual = { z = { fg = colors.black, bg = colors.yellow } },
+        replace = { z = { fg = colors.black, bg = colors.green } },
+      }
+      local options = {
+        theme = theme,
+        component_separators = { left = ' ', right = ' ' },
+        section_separators = { left = ' ', right = ' ' }
+      }
+      local sections = {
+        lualine_a = { { 'branch', colored = false } },
+        lualine_b = { { 'diff', colored = false }, { 'diagnostics', colored = false, always_visible = true }, { 'g:coc_status', colored = false, always_visible = true } },
+        lualine_c = { { 'filename', path = 1 },
+        { navic.get_location, cond = navic.is_available }
+      },
+      lualine_x = {},
+      lualine_y = { 'filetype', 'encoding', 'fileformat' },
+      lualine_z = { 'mode' }
+    }
+    local inactive_sections = {
+      lualine_c = { { 'filename', path = 1 } }
+    }
+
+    require('lualine').setup { options = options, sections = sections, inactive_sections = inactive_sections }
+  end
+},
 }
