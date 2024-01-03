489b321e77617756c0e19a64044dfe22c0570fbc
kazu914 <nkz0914ssk@gmail.com>
2024-01-03 21:21:55 +0900

    (zsh) remove unused aliases

--- zsh/99_aliases.zsh
+++ zsh/99_aliases.zsh
@@ -1,13 +1,3 @@
-# for atcoder
-if builtin command -v oj > /dev/null && builtin command -v acc >/dev/null;then
-  alias act="oj t -c 'pypy3 main.py'"
-  alias acs="acc s main.py -- --guess-python-interpreter pypy"
-fi
-
-if builtin command -v xdg-open > /dev/null; then
-  alias open='xdg-open'
-fi
-
 alias loadzshrc='source $HOME/.zshrc'
 alias vim="nvim"
 alias rg="rg --smart-case"
@@ -16,10 +6,6 @@ alias ls="lsd"
 alias ll="lsd -la"
 alias tree='lsd --tree'
 alias gbs='git branch -u origin/$(git rev-parse --abbrev-ref HEAD)'
-
-delombok () {
-  java -jar ~/.local/share/nvim/lsp_servers/jdtls/lombok.jar delombok -f pretty -p $1| bat -l java
-}
 
 # cd -> auto ls
 cd (){
